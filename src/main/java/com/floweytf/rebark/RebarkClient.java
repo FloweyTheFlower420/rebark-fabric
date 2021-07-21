1936c6dfa5f78d07efa66de02a064bb5d0270cc9
FloweyTheFlower420 <guywithanaxe42@gmail.com>
2021-07-21 14:14:36 -0500

    Bug patches

---
+++ src/main/java/com/floweytf/rebark/RebarkClient.java
@@ -0,0 +1,22 @@
+package com.floweytf.rebark;
+
+import com.floweytf.rebark.mixin.AxeItemAccessor;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class RebarkClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        ClientPlayConnectionEvents.JOIN.register((packetListener, packetSender, mc) -> {
+            if (BarkItem.UNSTRIP == null) {
+                BarkItem.UNSTRIP = AxeItemAccessor.getStrip().entrySet()
+                    .stream()
+                    .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
+            }
+        });
+    }
+}
